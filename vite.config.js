64d24ec0701f88f2f9c9164c411f501927f15d9e
natalia bolozan <bolozan.natalia@gmail.com>
2024-02-28 16:47:55 +0100

    init

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,24 @@
-// import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   basic: `/vue-sneakers-final/`,
-//   plugins: [
-//     vue(),
-//     vueJsx(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// })
-
-const {defineConfig} =require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: `/vue-sneakers-final/`,
-  transpileDependencies: true
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
+
+// const { defineConfig } = require('@vue/cli-service')
+// export default defineConfig({
+//   publicPath: `/vue-sneakers-final/`,
+//   transpileDependencies: true
+// })
